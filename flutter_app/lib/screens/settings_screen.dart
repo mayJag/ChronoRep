55a85dc20d608c5478efdc35440340d0d1257533
mayJag <jaggachinmay4@gmail.com>
2026-07-04 19:26:21 +0530

    chore: bump version to 1.6.1

--- flutter_app/lib/screens/settings_screen.dart
+++ flutter_app/lib/screens/settings_screen.dart
@@ -134,7 +134,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
 
           const SizedBox(height: 24),
           Center(
-            child: Text('ChronoRep v1.6.0',
+            child: Text('ChronoRep v1.6.1',
                 style: TextStyle(
                     fontSize: 12,
                     letterSpacing: 1,
